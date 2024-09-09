d8108b5a625d289808d36820bf6eafcbd6b6224f
wangxh <wangxiaohong@kuihai.net>
2024-09-09 13:57:39 +0800

    first commit

---
+++ src/main/java/io/github/xhom/Main.java
@@ -0,0 +1,11 @@
+package io.github.xhom;
+
+/**
+ * @author visy.wang
+ * @date 2024/9/9 13:55
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
